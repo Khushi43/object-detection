4dfead1509ea72a29b65f8b653a58891810a7f58
Khushi43 <126304943+Khushi43@users.noreply.github.com>
2023-09-23 20:57:00 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function findBedroom(){
+    window.location.replace("bedroom.html");
+}
+
+function  findTvandAc(){
+    window.location.replace("tv&ac.html");
+}
+
+function findDesk(){
+    window.location.replace("desk.html");
+}
+
+function findBottle(){
+    window.location.replace("bottle.html");
+}
+
+function findFruitBasket(){
+    window.location.replace("fruitbasket.html");
+}
